cf423e26e2853cf0f45f37f1c95dc23e512ab088
Your Name <you@example.com>
2019-09-15 00:23:20 +0530

    Session Tracking Done

--- app/src/main/java/com/example/MaaKaKhana/Home_screen.java
+++ app/src/main/java/com/example/MaaKaKhana/Home_screen.java
@@ -2,7 +2,9 @@ package com.example.MaaKaKhana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home_screen extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Home_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    public void logout(View view) {
+        getSharedPreferences("session",MODE_PRIVATE).edit().clear().apply();
+        Intent intent =new Intent(Home_screen.this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
