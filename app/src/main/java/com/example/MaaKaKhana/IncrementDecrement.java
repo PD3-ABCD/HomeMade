4f7fab14007e6db0fc9cdd5a8f32c1b57d17f705
Your Name <you@example.com>
2019-10-13 07:11:38 +0530

    Tried Total

---
+++ app/src/main/java/com/example/MaaKaKhana/IncrementDecrement.java
@@ -0,0 +1,37 @@
+package com.example.MaaKaKhana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+
+public class IncrementDecrement extends AppCompatActivity {
+
+    ElegantNumberButton elegantNumberButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cart_data);
+
+        elegantNumberButton = (ElegantNumberButton)findViewById(R.id.myButton);
+        elegantNumberButton.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num=elegantNumberButton.getNumber();
+                //Log.e("Number",num);
+                Toast.makeText(IncrementDecrement.this, "Quantity is: "+num, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    int ret_count(){
+        String num=elegantNumberButton.getNumber();
+        int n = Integer.parseInt(num);
+        return n;
+    }
+}
--- app/src/main/java/com/example/MaaKaKhana/ui/cart/CartFragment.java
+++ app/src/main/java/com/example/MaaKaKhana/ui/cart/CartFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.MaaKaKhana.IncrementDecrement;
 import com.example.MaaKaKhana.MeraUPI;
 import com.example.MaaKaKhana.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,11 +37,17 @@ public class CartFragment extends Fragment {
     private ListData2 l;
     private LinearLayout ll;
     private Button b1;
+    private IncrementDecrement icdc;
+    private int total=0;
+    private int count;
+    private TextView t_price;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_cart, container, false);
 
+
+        //Recycler code
         rv2 = (RecyclerView) view.findViewById(R.id.rev2);
         rv2.setHasFixedSize(true);
         rv2.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -54,6 +62,16 @@ public class CartFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+
+        //Total coount
+        adapter2=new MyAdapter2(listData2);
+        t_price=(TextView)view.findViewById(R.id.priceTitle);
+        count=adapter2.getItemCount();
+        for (int i=0;i<count;i++){
+            total+=l.getFood_price();
+        }
+        t_price.setText("Rs. "+total);
 
         final DatabaseReference nm = FirebaseDatabase.getInstance().getReference().child("Registration").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("My Cart");
         nm.addListenerForSingleValueEvent(new ValueEventListener() {
